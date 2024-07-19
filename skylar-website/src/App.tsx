e19cfc881647aafad9ca6ca4ab5e16dc0aa67ea9
sji <sji@uwaterloo.ca>
2024-07-18 21:44:34 -0400

    added scrolling functionality to navbar

--- skylar-website/src/App.tsx
+++ skylar-website/src/App.tsx
@@ -1,24 +1,47 @@
-import './App.css'
-import NavBar from './components/Navbar/Navbar.js';
+import React, { useRef } from 'react';
+import './App.css';
+import NavBar from './components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
-import HomePage from './pages/home.js';
-import AboutPage from './pages/about.js';
-import WorkExperiencePage from './pages/workExperience.js';
-import ProjectsPage from './pages/projects.js';
-import ArticlesPage from './pages/articles.js';
-import Footer from './components/Footer/footer.js';
+import HomePage from './pages/home';
+import AboutPage from './pages/about';
+import WorkExperiencePage from './pages/workExperience';
+import ProjectsPage from './pages/projects';
+import ArticlesPage from './pages/articles';
+import Footer from './components/Footer/footer';
 
 function App() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const experienceRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const articlesRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.warn('Ref is null:', ref);
+    }
+  };
+
   return (
     <div className="w-screen bg-gradient-to-b from-pastelBlue to-pastelPurple">
       <BrowserRouter>
-        <NavBar />
+        <NavBar handleScroll={handleScroll} refs={{ aboutRef, experienceRef, projectsRef, articlesRef }} />
       </BrowserRouter>
+
       <HomePage />
-      <AboutPage />
-      <WorkExperiencePage />
-      <ProjectsPage />
-      <ArticlesPage />
+      <div ref={aboutRef}>
+        <AboutPage />
+      </div>
+      <div ref={experienceRef}>
+        <WorkExperiencePage />
+      </div>
+      <div ref={projectsRef}>
+        <ProjectsPage />
+      </div>
+      <div ref={articlesRef}>
+        <ArticlesPage />
+      </div>
       <Footer />
     </div>
   );
--- skylar-website/src/components/Navbar/Navbar.tsx
+++ skylar-website/src/components/Navbar/Navbar.tsx
@@ -1,159 +1,171 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import { useLocation } from 'react-router-dom';
 
 const MenuIcon = (props: any) => {
-    return (
-        <div {...props}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="currentColor"
-            >
-                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-            </svg>
-        </div>
-    );
+  return (
+    <div {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="currentColor"
+      >
+        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+      </svg>
+    </div>
+  );
 };
+
 const CloseIcon = (props: any) => {
-    return (
-        <div {...props}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="currentColor"
-            >
-                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-            </svg>
-        </div>
-    );
+  return (
+    <div {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="currentColor"
+      >
+        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+      </svg>
+    </div>
+  );
 };
 
-const NavbarContent: React.FunctionComponent<any> = ({ children, ...rest }) => {
-    let [open, setOpen] = React.useState(false);
-    return (
-        <>
-            <div className={open ? styles.open : styles.closed} {...rest}>
-                <div className={styles.lgOnly}>
-                    <div className={styles.collapsable}>{children}</div>
-                </div>
-                <div
-                    onClick={() => {
-                        setOpen(!open);
-                    }}
-                    className={styles.smOnly}
-                >
-                    <MenuIcon
-                        style={{
-                            transform: 'translateX(90%)',
-                            marginTop: '0.75rem',
-                            color: '#000'
-                        }}
-                    />
-                </div>
-                <div
-                    className={styles.overlay}
-                    onClick={() => setOpen(false)}
-                ></div>
-                <div className={styles.drawer}>
-                    <div className={styles.drawerContent}>
-                        <CloseIcon
-                            onClick={() => setOpen(false)}
-                            style={{
-                                position: 'absolute',
-                                top: 0,
-                                right: 0,
-                                margin: '1rem',
-                                color: '#000'
-                            }}
-                        />
-                        {children}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+type NavbarContentProps = {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
 };
 
-export const Navbar: React.FunctionComponent<any> = ({}) => {
-    const location = useLocation();
-    return (
-        <div className={styles.navbar}>
-            <a
-                href="/#home"
-                style={{
-                    position: 'absolute',
-                    left: 0,
-                    top: 0,
-                    padding: '1rem',
-                    fontSize: '24px',
-                    fontWeight: '660',
-                    textTransform: 'none',
-                    color: 'white',
-                }}
-            >
-                <div className="bg-white rounded-full">
-                <img className="w-12 h-12 p-2" src="/favicon.png"></img>
-                </div>
-            </a>
-            <NavbarContent style={{ flex: 1 }}>
-                <a
-                    className={styles.navbarEntry}
-                    style={{
-                        fontWeight:
-                            location.pathname == '/#home' ? '800' : '600',
-                    }}
-                    href="/#home"
-                >
-                    Home
-                </a>
-                <a
-                    className={styles.navbarEntry}
-                    style={{
-                        fontWeight:
-                            location.pathname == '/#about' ? '800' : '600',
-                    }}
-                    href="/#about"
-                >
-                    About
-                </a>
-                <a
-                    className={styles.navbarEntry}
-                    style={{
-                        fontWeight:
-                            location.pathname == '/#experience' ? '800' : '600',
-                    }}
-                    href="/#experience"
-                >
-                    Experience
-                </a>
-                <a
-                    className={styles.navbarEntry}
-                    style={{
-                        fontWeight:
-                            location.pathname == '/#projects' ? '800' : '600',
-                    }}
-                    href="/#projects"
-                >
-                    Projects
-                </a>
-                <a
-                    className={styles.navbarEntry}
-                    style={{
-                        fontWeight:
-                            location.pathname == '/#articles' ? '800' : '600',
-                    }}
-                    href="/#articles"
-                >
-                    Articles
-                </a>
-            </NavbarContent>
+const NavbarContent: React.FC<NavbarContentProps> = ({ children, style, ...rest }) => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <>
+      <div className={open ? styles.open : styles.closed} {...rest}>
+        <div className={styles.lgOnly}>
+          <div className={styles.collapsable} style={style}>
+            {children}
+          </div>
         </div>
-    );
+        <div
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className={styles.smOnly}
+        >
+          <MenuIcon
+            style={{
+              transform: 'translateX(90%)',
+              marginTop: '0.75rem',
+              color: '#000',
+            }}
+          />
+        </div>
+        <div className={styles.overlay} onClick={() => setOpen(false)}></div>
+        <div className={styles.drawer}>
+          <div className={styles.drawerContent}>
+            <CloseIcon
+              onClick={() => setOpen(false)}
+              style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                margin: '1rem',
+                color: '#000',
+              }}
+            />
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+type NavbarProps = {
+  handleScroll: (ref: React.RefObject<HTMLDivElement>) => void;
+  refs: {
+    aboutRef: React.RefObject<HTMLDivElement>;
+    experienceRef: React.RefObject<HTMLDivElement>;
+    projectsRef: React.RefObject<HTMLDivElement>;
+    articlesRef: React.RefObject<HTMLDivElement>;
+  };
+};
+
+export const Navbar: React.FC<NavbarProps> = ({ handleScroll, refs }) => {
+  return (
+    <div className={styles.navbar}>
+      <a
+        href="#home"
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          padding: '1rem',
+          fontSize: '24px',
+          fontWeight: '660',
+          textTransform: 'none',
+          color: 'white',
+        }}
+        onClick={(e) => {
+          e.preventDefault();
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }}
+      >
+        <div className="bg-white rounded-full">
+          <img className="w-12 h-12 p-2" src="/favicon.png" alt="Logo" />
+        </div>
+      </a>
+      <NavbarContent style={{ flex: 1 }}>
+        <a
+          className={styles.navbarEntry}
+          onClick={(e) => {
+            e.preventDefault();
+            window.scrollTo({ top: 0, behavior: 'smooth' }); // scroll to the top of the page
+          }}
+        >
+          Home
+        </a>
+        <a
+          className={styles.navbarEntry}
+          onClick={(e) => {
+            e.preventDefault();
+            handleScroll(refs.aboutRef);
+          }}
+        >
+          About
+        </a>
+        <a
+          className={styles.navbarEntry}
+          onClick={(e) => {
+            e.preventDefault();
+            handleScroll(refs.experienceRef);
+          }}
+        >
+          Experience
+        </a>
+        <a
+          className={styles.navbarEntry}
+          onClick={(e) => {
+            e.preventDefault();
+            handleScroll(refs.projectsRef);
+          }}
+        >
+          Projects
+        </a>
+        <a
+          className={styles.navbarEntry}
+          onClick={(e) => {
+            e.preventDefault();
+            handleScroll(refs.articlesRef);
+          }}
+        >
+          Articles
+        </a>
+      </NavbarContent>
+    </div>
+  );
 };
 
 export default Navbar;
