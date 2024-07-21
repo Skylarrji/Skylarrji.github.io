72e7376c7739a99da2dc20864f361f497f64c124
sji <sji@uwaterloo.ca>
2024-07-21 14:22:58 -0400

    cleaned up navbar

--- skylar-website/src/components/Navbar/Navbar.tsx
+++ skylar-website/src/components/Navbar/Navbar.tsx
@@ -1,88 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
 
-const MenuIcon = (props: any) => {
-  return (
-    <div {...props}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="24px"
-        viewBox="0 -960 960 960"
-        width="24px"
-        fill="currentColor"
-      >
-        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-      </svg>
-    </div>
-  );
-};
-
-const CloseIcon = (props: any) => {
-  return (
-    <div {...props}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="24px"
-        viewBox="0 -960 960 960"
-        width="24px"
-        fill="currentColor"
-      >
-        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-      </svg>
-    </div>
-  );
-};
-
-type NavbarContentProps = {
-  children: React.ReactNode;
-  style?: React.CSSProperties;
-};
-
-const NavbarContent: React.FC<NavbarContentProps> = ({ children, style, ...rest }) => {
-  const [open, setOpen] = React.useState(false);
-  return (
-    <>
-      <div className={open ? styles.open : styles.closed} {...rest}>
-        <div className={styles.lgOnly}>
-          <div className={styles.collapsable} style={style}>
-            {children}
-          </div>
-        </div>
-        <div
-          onClick={() => {
-            setOpen(!open);
-          }}
-          className={styles.smOnly}
-        >
-          <MenuIcon
-            style={{
-              transform: 'translateX(90%)',
-              marginTop: '0.75rem',
-              color: '#000',
-            }}
-          />
-        </div>
-        <div className={styles.overlay} onClick={() => setOpen(false)}></div>
-        <div className={styles.drawer}>
-          <div className={styles.drawerContent}>
-            <CloseIcon
-              onClick={() => setOpen(false)}
-              style={{
-                position: 'absolute',
-                top: 0,
-                right: 0,
-                margin: '1rem',
-                color: '#000',
-              }}
-            />
-            {children}
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
 type NavbarProps = {
   handleScroll: (ref: React.RefObject<HTMLDivElement>) => void;
   refs: {
@@ -134,73 +52,71 @@ export const Navbar: React.FC<NavbarProps> = ({ handleScroll, refs }) => {
           position: 'absolute',
           left: 0,
           top: 0,
-          padding: '0.5rem',
-          fontSize: '24px',
-          fontWeight: '660',
-          textTransform: 'none',
-          color: 'white',
+          padding: '0.75rem',
         }}
         onClick={(e) => {
           e.preventDefault();
           window.scrollTo({ top: 0, behavior: 'smooth' });
         }}
       >
-        <div className="bg-white rounded-full">
+        <div className="bg-white rounded-full hidden lg:block md:block sm:block">
           <img className="w-12 h-12 p-2" src="/favicon.png" alt="Logo" />
         </div>
       </a>
-      <NavbarContent style={{ flex: 1 }}>
-        <a
-          className={`${styles.navbarEntry} ${currentSection === 'home' ? styles.active : ''}`}
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentSection('home');
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-          }}
-        >
-          Home
-        </a>
-        <a
-          className={`${styles.navbarEntry} ${currentSection === 'about' ? styles.active : ''}`}
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentSection('about');
-            handleScroll(refs.aboutRef);
-          }}
-        >
-          About
-        </a>
-        <a
-          className={`${styles.navbarEntry} ${currentSection === 'experience' ? styles.active : ''}`}
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentSection('experience');
-            handleScroll(refs.experienceRef);
-          }}
-        >
-          Experience
-        </a>
-        <a
-          className={`${styles.navbarEntry} ${currentSection === 'projects' ? styles.active : ''}`}
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentSection('projects');
-            handleScroll(refs.projectsRef);
-          }}
-        >
-          Projects
-        </a>
-        <a
-          className={`${styles.navbarEntry} ${currentSection === 'articles' ? styles.active : ''}`}
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentSection('articles');
-            handleScroll(refs.articlesRef);
-          }}
-        >
-          Articles
-        </a>
-      </NavbarContent>
+        <div className="block">
+          <div className="flex justify-end gap-1 sm:gap-4 md:gap-4 lg:gap-4 mr-4 flex-1">
+            <a
+              className={`${styles.navbarItem} ${currentSection === 'home' ? styles.active : ''}`}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentSection('home');
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+              }}
+            >
+              Home
+            </a>
+            <a
+              className={`${styles.navbarItem} ${currentSection === 'about' ? styles.active : ''}`}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentSection('about');
+                handleScroll(refs.aboutRef);
+              }}
+            >
+              About
+            </a>
+            <a
+              className={`${styles.navbarItem} ${currentSection === 'experience' ? styles.active : ''}`}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentSection('experience');
+                handleScroll(refs.experienceRef);
+              }}
+            >
+              Experience
+            </a>
+            <a
+              className={`${styles.navbarItem} ${currentSection === 'projects' ? styles.active : ''}`}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentSection('projects');
+                handleScroll(refs.projectsRef);
+              }}
+            >
+              Projects
+            </a>
+            <a
+              className={`${styles.navbarItem} ${currentSection === 'articles' ? styles.active : ''}`}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentSection('articles');
+                handleScroll(refs.articlesRef);
+              }}
+            >
+              Articles
+            </a>
+          </div>
+        </div>
     </div>
   );
 };
