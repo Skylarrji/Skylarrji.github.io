2e95705844bc60636c798b4bdc63f38e235cc791
sji <sji@uwaterloo.ca>
2024-07-22 21:53:54 -0400

    final mobile changes

--- src/pages/workExperience.tsx
+++ src/pages/workExperience.tsx
@@ -30,11 +30,11 @@ const WorkExperiencePage = () => {
                 <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center justify-center">
                     {jobs && jobs[0] && (
                         <>
-                            <div className="flex sm:flex-row flex-col ml-5 mr-5 lg:mr-5 gap-5 p-5 py-7 lg:p-5 md:py-3 md:px-5 sm:px-5 sm:py-3 bg-white items-center justify-center rounded-2xl min-w-[425px] max-w-[425px] sm:min-w-[625px] sm:max-w-[625px] md:min-w-[825px] md:max-w-[825px] lg:min-w-[900px] lg:max-w-[900px]">
-                                <img className="min-w-[370px] max-w-[370px] sm:min-w-[250px] sm:max-w-[250px] md:min-w-[250px] md:max-w-[250px] lg:min-w-[325px] lg:max-w-[325px] rounded-2xl" src={jobs[0].imageLink} alt={`${jobs[0].companyName} logo`} />
+                            <div className="flex sm:flex-row flex-col ml-5 mr-5 lg:mr-5 gap-5 p-5 py-7 lg:p-5 md:py-3 md:px-5 sm:px-5 sm:py-3 bg-white items-center justify-center rounded-2xl min-w-[85vw] max-w-[85vw] sm:min-w-[625px] sm:max-w-[625px] md:min-w-[825px] md:max-w-[825px] lg:min-w-[900px] lg:max-w-[900px]">
+                                <img className="min-w-[70vw] max-w-[70vw] sm:min-w-[250px] sm:max-w-[250px] md:min-w-[250px] md:max-w-[250px] lg:min-w-[325px] lg:max-w-[325px] rounded-2xl" src={jobs[0].imageLink} alt={`${jobs[0].companyName} logo`} />
                                 <div className="flex flex-col gap-1 sm:items-start sm:justify-start justify-center items-center">
                                     <h4 className="text-blue-dark font-bold text-xl lg:text-xl md:text-lg sm:text-base">{jobs[0].companyName}</h4>
-                                    <h5 className="text-blue-dark font-semibold text-lg mb-1 sm:mb-0 lg:text-lg md:text-base sm:text-sm">{jobs[0].role}</h5>
+                                    <h5 className="text-blue-dark font-semibold text-lg mb-1 text-center sm:text-left sm:mb-0 lg:text-lg md:text-base sm:text-sm">{jobs[0].role}</h5>
                                     <div className="flex flex-wrap gap-1.5 sm:justify-start justify-center items-center mb-1 sm:mb-0">
                                         {jobs[0].skills.map((skill) => (
                                             <Tag key={skill} name={skill}/>
@@ -52,8 +52,8 @@ const WorkExperiencePage = () => {
                     {jobs && jobs[1] && (
                         <>
                             <img className="hidden lg:block w-1/4" src={'/kite.svg'} alt="Decoration" />
-                            <div className="flex sm:flex-row flex-col ml-5 mr-5 lg:mr-5 gap-5 p-5 py-7 lg:p-5 md:py-3 md:px-5 sm:px-5 sm:py-3 bg-white items-center justify-center rounded-2xl min-w-[425px] max-w-[425px] sm:min-w-[625px] sm:max-w-[625px] md:min-w-[825px] md:max-w-[825px] lg:min-w-[900px] lg:max-w-[900px]">
-                                <img className="min-w-[370px] max-w-[370px] sm:min-w-[250px] sm:max-w-[250px] md:min-w-[250px] md:max-w-[250px] lg:min-w-[325px] lg:max-w-[325px] rounded-2xl" src={jobs[1].imageLink} alt={`${jobs[1].companyName} logo`} />
+                            <div className="flex sm:flex-row flex-col ml-5 mr-5 lg:mr-5 gap-5 p-5 py-7 lg:p-5 md:py-3 md:px-5 sm:px-5 sm:py-3 bg-white items-center justify-center rounded-2xl min-w-[85vw] max-w-[85vw] sm:min-w-[625px] sm:max-w-[625px] md:min-w-[825px] md:max-w-[825px] lg:min-w-[900px] lg:max-w-[900px]">
+                                <img className="min-w-[70vw] max-w-[70vw] sm:min-w-[250px] sm:max-w-[250px] md:min-w-[250px] md:max-w-[250px] lg:min-w-[325px] lg:max-w-[325px] rounded-2xl" src={jobs[1].imageLink} alt={`${jobs[1].companyName} logo`} />
                                 <div className="flex flex-col gap-1 sm:items-start sm:justify-start justify-center items-center">
                                     <h4 className="text-blue-dark font-bold text-xl lg:text-xl md:text-lg sm:text-base">{jobs[1].companyName}</h4>
                                     <h5 className="text-blue-dark font-semibold text-lg mb-1 text-center sm:text-left sm:mb-0 lg:text-lg md:text-base sm:text-sm">{jobs[1].role}</h5>
