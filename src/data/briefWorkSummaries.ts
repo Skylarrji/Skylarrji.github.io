f145d42b09108bfd48b103cb3e6573977bf88ff6
Skylar Ji <t-jiskylar@microsoft.com>
2026-07-21 16:05:30 -0700

    reduced where i've worked to 3 experiences

--- src/data/briefWorkSummaries.ts
+++ src/data/briefWorkSummaries.ts
@@ -29,33 +29,5 @@ export async function fetchWorkSummaries(): Promise<WorkSummary[]> {
       location: 'Waterloo, ON',
       role: 'Undergraduate Research Assistant',
     },
-    {
-      date: "January – May 2025",
-      companyName: "Microsoft",
-      companyImage: "/microsoft.png",
-      location: "Vancouver, BC",
-      role: "Software Engineer",
-    },
-    {
-      date: "May – August 2024",
-      companyName: "Games Institute",
-      companyImage: "/gamesInstitute.png",
-      location: "Waterloo, ON",
-      role: "Fullstack Developer",
-    },
-    {
-      date: "May – August 2023",
-      companyName: "Royal Bank of Canada",
-      companyImage: "/rbc.svg",
-      location: "Toronto, ON",
-      role: "DevOps Engineer",
-    },
-    {
-      date: "July – August 2022",
-      companyName: "Royal Bank of Canada",
-      companyImage: "/rbc.svg",
-      location: "Toronto, ON",
-      role: "Innovation Developer - Summer Tech Labs",
-    },
   ];
 }
